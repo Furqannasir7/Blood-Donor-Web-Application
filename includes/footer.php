209f731bdaa42a37ad88f3475fafe4ffdc89f819
MalikHusnain <Husnainmalik0235@gmail.com>
2019-06-27 20:09:29 +0500

    header&&footer includes

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Developed by Pythons</p>
+    </div>
+    <!-- /.container -->
+</footer>
---
+++ includes/slider.php
@@ -0,0 +1,25 @@
+<header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <!-- Slide One - Set the background image for this slide in the line below -->
+            <div class="carousel-item active" style="background-image: url('https://cdn.pixabay.com/photo/2019/02/14/12/04/blood-3996485_960_720.jpg')"></div>
+            <!-- Slide Two - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('https://media.gettyimages.com/photos/pouches-of-donated-blood-in-hospital-picture-id482145647?s=2048x2048')"></div>
+            <!-- Slide Three - Set the background image for this slide in the line below -->
+            <div class="carousel-item" style="background-image: url('https://media.gettyimages.com/photos/young-woman-is-going-to-donate-blood-in-blood-bank-picture-id626838958?s=2048x2048')"></div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</header>
